0b4bfc3aac55b436370b4ed47f991f9e12f0da2e
7Arham <loader05g@gmail.com>
2026-08-01 07:35:56 +0530

    Add files via upload

---
+++ valid parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.length();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char top=st.top();
+                if((ch==')' && top=='(')||(ch==']' && top=='[')||(ch=='}' && top=='{')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+  return st.empty();  }
+};
